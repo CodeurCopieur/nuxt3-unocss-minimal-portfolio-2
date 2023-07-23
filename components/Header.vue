a03cd2acca2953b95320cd9de7a11515f10cc3dc
Widdy LOUIS <louisws@outlook.fr>
2023-07-23 23:26:05 +0200

    integration component header

---
+++ components/Header.vue
@@ -0,0 +1,14 @@
+<script setup>
+  
+</script>
+
+<template>
+ <header class="w-full fixed py-6 top-0 bg-gray-50/10 backdrop-blur-md">
+    <div class="flex mx-auto w-full px-4 items-center justify-between xl:w-screen-xl xl:px-0">
+      <nav class="flex gap-4">
+        <NuxtLink to="/" class="font-serif text-gray-800">Home</NuxtLink>
+        <NuxtLink to="/works" class="font-serif text-gray-800">Works</NuxtLink>
+      </nav>
+    </div>
+ </header>
+</template>
