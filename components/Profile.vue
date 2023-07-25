a8005c9cd0ef68cadbe74a43f8bc95422d26d589
Widdy LOUIS <louisws@outlook.fr>
2023-07-25 22:08:07 +0200

    Create Profile.vue

---
+++ components/Profile.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="mx-auto w-full grid grid-cols-1 gap-4 xl:max-w-screen-xl xl:gap-10 xl:grid-cols-2">
+    <div class="flex flex-col gap-2 justify-center">
+      <h1 class="font-serif text-left text-5xl text-gray-800 xl:text-7xl">Profile</h1>
+    </div>
+  </div>
+</template>
