b0edd2e906b29cea8d9d08dfae4208d656381a6c
Widdy LOUIS <louisws@outlook.fr>
2023-07-26 23:18:22 +0200

    Update Profile.vue

--- components/Profile.vue
+++ components/Profile.vue
@@ -1,7 +1,20 @@
 <template>
-  <div class="mx-auto w-full grid grid-cols-1 gap-4 xl:max-w-screen-xl xl:gap-10 xl:grid-cols-2">
-    <div class="flex flex-col gap-2 justify-center">
-      <h1 class="font-serif text-left text-5xl text-gray-800 xl:text-7xl">Profile</h1>
+  <div class="mx-auto mt-54 w-full grid grid-cols-1 gap-4 xl:max-w-screen-xl xl:gap-10 xl:grid-cols-2">
+    <div class="flex flex-col px-24 gap-2 justify-center">
+      <h1 class="font-serif text-left text-5xl text-gray-800 xl:text-7xl">CodeurCopieur</h1>
+      <h2 class="font-serif text-left text-3xl text-gray-800/80 xl:text-right xl:text-5xl">Développeur</h2>
+      <p class="font-serif mt-4 text-xl text-gray-800/70">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+        Magnam asperiores, quisquam, fugiat tempore possimus perferendis dolore aliquam minima quis atque odio et porro natus quos ut consequuntur totam ab quae!
+      </p>
+      <ul class="flex gap-4">
+        <li><a href="" class="font-serif text-gray-800 underline">Github</a></li>
+        <li><a href="" class="font-serif text-gray-800 underline">CodeSandbox</a></li>
+        <li><a href="" class="font-serif text-gray-800 underline">Contact</a></li>
+      </ul>
+    </div>
+    <div class="w-full">
+    <img src="https://avatars.githubusercontent.com/u/36073831?v=4" class="border mx-auto object-cover h-160 w-1/2 xl:w-full animate-fade-in" width="200" height="500" alt="">
     </div>
   </div>
 </template>
