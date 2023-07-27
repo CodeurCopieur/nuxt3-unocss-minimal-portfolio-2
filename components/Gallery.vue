d490926a90eb9e1a91d24ef07ac589056cf581f3
Widdy LOUIS <louisws@outlook.fr>
2023-07-27 23:13:46 +0200

    Create Gallery.vue

---
+++ components/Gallery.vue
@@ -0,0 +1,7 @@
+<template>
+   <div class="mx-auto w-full px-4 sm:mt-0 xl:w-full xl: px-0">
+    <h1 class="font-serif mx-auto max-w-screen-xl mt-20 text-left text-5xl text-gray-800 xl:text-7xl">
+      Projet récent
+    </h1>
+  </div>
+</template>
