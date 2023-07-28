a426d102e71f3e5200623ca0b9d18c984ab559a9
Widdy LOUIS <louisws@outlook.fr>
2023-07-29 00:07:12 +0200

    Update Gallery.vue

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -3,5 +3,12 @@
     <h1 class="font-serif mx-auto max-w-screen-xl mt-20 text-left text-5xl text-gray-800 xl:text-7xl">
       Projet récent
     </h1>
+    <div class="flex mt-10 p-10 gap-10 snap-x snap-mandatory relative overflow-x-auto">
+      <template>
+        <div class="cursor-pointer flex flex-col snap-end overflow-visible shrink-0">
+          <!-- Photos -->
+        </div>
+      </template>
+    </div>
   </div>
 </template>
