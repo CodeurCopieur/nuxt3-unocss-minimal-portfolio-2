2ce1490860ac21efeb874f217104c54cc870ac37
Widdy LOUIS <louisws@outlook.fr>
2023-07-29 22:57:58 +0200

    Create Photo.vue

---
+++ components/Photo.vue
@@ -0,0 +1,44 @@
+<script setup>
+  defineProps({
+    photo: {
+      type: String,
+      default: '/'
+    },
+    orientation: {
+      type: String,
+      default: 'portrait'
+    },
+    index: {
+      type: Number,
+      default: 0
+    },
+    title: {
+      type: String,
+      default: 'Amazing photo'
+    },
+    year: {
+      type: Number,
+      default: new Date().getFullYear()
+    },
+  });
+</script>
+
+<template>
+  <div class="border bg-gray-50 border-gray-800 shadow-md p-2 transform-gpu transform-all ease-in-out duration-300 will-change-transform hover:shadow-lg hover:rotate-2">
+    <img width="220" height="340" class="object-cover object-center animate-fade-in" alt=""
+    :src="photo"  
+    :class="{'w-85 h-125': orientation === 'portrait', 'w-145 h-125': orientation === 'landscape' }">
+
+    <div class="flex font-serif mt-2 justify-between items-center">
+      <span class="text-base text-gray-400 inline">
+        /0{{ index }}
+      </span>
+      <span class="text-xl text-gray-800">
+        {{ title }}
+      </span>
+      <div class="font-serif text-right text-base w-full text-gray-400">
+        {{  year }}
+      </div>
+    </div>
+  </div>
+</template>
