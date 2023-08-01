4909899723fde6dd6bafdeb306ad2a8fb9b2834a
Widdy LOUIS <louisws@outlook.fr>
2023-08-01 19:31:56 +0200

    Update Gallery.vue

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -2,14 +2,14 @@
   const photos = [
     {url: '/gallery/01.jpg', title: "Title Generic 1", orientation: 'portrait', year: 2023},
     {url: '/gallery/02.jpg', title: "Title Generic 2", orientation: 'portrait', year: 2023},
-    {url: '/gallery/03.jpg', title: "Title Generic 3", orientation: 'portrait', year: 2023},
+    {url: '/gallery/03.jpg', title: "Title Generic 3", orientation: 'landscape', year: 2023},
     {url: '/gallery/04.jpg', title: "Title Generic 4", orientation: 'portrait', year: 2023},
-    {url: '/gallery/05.jpg', title: "Title Generic 5", orientation: 'portrait', year: 2023},
+    {url: '/gallery/05.jpg', title: "Title Generic 5", orientation: 'landscape', year: 2023},
     {url: '/gallery/06.jpg', title: "Title Generic 6", orientation: 'portrait', year: 2022},
-    {url: '/gallery/07.jpg', title: "Title Generic 7", orientation: 'portrait', year: 2022},
+    {url: '/gallery/07.jpg', title: "Title Generic 7", orientation: 'landscape', year: 2022},
     {url: '/gallery/08.jpg', title: "Title Generic 8", orientation: 'portrait', year: 2021},
     {url: '/gallery/09.jpg', title: "Title Generic 9", orientation: 'portrait', year: 2021},
-    {url: '/gallery/10.jpg', title: "Title Generic 10", orientation: 'portrait', year: 2020},
+    {url: '/gallery/10.jpg', title: "Title Generic 10", orientation: 'landscape', year: 2020},
     ];
 </script>
 <template>
